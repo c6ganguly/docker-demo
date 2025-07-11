cfb70c14a1167b340d41750d6e6fb78a1b5265e4
c6ganguly <59741462+c6ganguly@users.noreply.github.com>
2025-07-11 23:23:09 +0530

    Code Change
    Code Change

---
+++ src/main/java/com/example/rest/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Welcome to your first RESTful Spring Boot app!";
+    }
+}
